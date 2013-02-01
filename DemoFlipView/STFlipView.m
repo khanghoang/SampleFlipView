2ee8971f8365598502438f48e6ca6a43fd358358
Hoang Trieu Khang <hoangtrieukhang@gmail.com>
2013-02-01 14:34:53 +0700

    - Initalization

--- DemoFlipView/STFlipView.m
+++ DemoFlipView/STFlipView.m
@@ -7,6 +7,14 @@
 //
 
 #import "STFlipView.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface STFlipView()
+
+@property (assign, nonatomic) BOOL fullScreen;
+@property (assign, nonatomic) CGRect originFrame;
+
+@end
 
 @implementation STFlipView
 
@@ -15,6 +23,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        self.fullScreen = YES;
     }
     return self;
 }
@@ -28,4 +37,21 @@
 }
 */
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    
+    if(self.originFrame.size.height == 0 && self.originFrame.size.width == 0)
+        self.originFrame = self.frame;
+    
+    [UIView transitionWithView:self duration:1 options:UIViewAnimationOptionTransitionFlipFromLeft animations:^{
+            if (self.fullScreen) {
+                self.frame = screenRect;
+            }else{
+                self.frame = self.originFrame;
+            }
+        } completion:^(BOOL finished) {
+            self.fullScreen = !self.fullScreen;
+    }];
+}
 @end
