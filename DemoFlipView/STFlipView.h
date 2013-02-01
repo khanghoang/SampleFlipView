bdc3bbb619f38ddb0a3b70a21cdffa55321af067
Hoang Trieu Khang <hoangtrieukhang@gmail.com>
2013-02-01 14:34:32 +0700

    - Initalization

---
+++ DemoFlipView/STFlipView.h
@@ -0,0 +1,13 @@
+//
+//  STFlipView.h
+//  DemoFlipView
+//
+//  Created by Trieu Khang on 2/1/13.
+//  Copyright (c) 2013 Hoang Trieu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STFlipView : UIView
+
+@end
---
+++ DemoFlipView/STFlipView.m
@@ -0,0 +1,31 @@
+//
+//  STFlipView.m
+//  DemoFlipView
+//
+//  Created by Trieu Khang on 2/1/13.
+//  Copyright (c) 2013 Hoang Trieu Khang. All rights reserved.
+//
+
+#import "STFlipView.h"
+
+@implementation STFlipView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
